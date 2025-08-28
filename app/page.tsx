e6d7267d72fdfcecad7435444038d1c06c18a98d
Mariam <my243898@gmail.com>
2025-08-28 23:08:43 +0100

    modified:   .gitignore 	modified:   app/page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
-import { redirect } from 'next/navigation';
+import Homescreen from '@/components/FootballScreen/Homescreen';
 
 export default function Home() {
-  redirect('/onboarding');
+  return <Homescreen />;
 }
